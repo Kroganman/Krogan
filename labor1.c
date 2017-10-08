1e6d3615c6e5bb2173a6b244c2fb9066874adeb4
Kroganman <31574752+Kroganman@users.noreply.github.com>
2017-10-08 22:26:29 +0300

    Add files via upload

---
+++ labor1.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    char opr;
+    float temp, kelvin, celsius, far;
+    if(argc==1)   /* Proverka na vvod shkali    */
+    {
+        opr=0;
+    }
+        else
+        {
+            if(argc==2)
+                opr=*argv[2];
+            if(argc>2)
+                printf("Error!");
+        }
+    temp=atof(argv[1]);
+    switch (opr)
+    {
+      case 'K':  /* Shkala Kelvina */
+      case 'k':
+        if(temp>0)
+        {
+           kelvin=temp;
+           far=1.8*(kelvin - 273.15) + 32;
+           celsius=kelvin -273.15;
+           printf("F: %.2f\nC: %.2f", far, celsius);
+        }
+        else
+           printf("Not lower than absolute zero!");
+        break;
+      case 'C': /* Shkala Celsiya */
+      case 'c':
+        if(temp>-273)
+        {
+           celsius=temp;
+           kelvin=celsius+273.15;
+           far=1.8* celsius + 32;
+           printf("F: %.2f\nK: %.2f", far, kelvin);
+        }
+        else
+           printf("Not lower than absolute zero!");
+        break;
+      case 'F': /* Shkala Farengeita */
+      case 'f':
+        if(temp>-459)
+        {
+           far=temp;
+           celsius=5 * (far-32) / 9;
+           kelvin=(5 * (far-32) / 9) +273.15;
+           printf("K: %.2f\nC: %.2f", kelvin, celsius);
+        }
+        else
+           printf("Not lower than absolute zero!");
+        break;
+      default: /* Shkala ne ykazana */
+        kelvin=temp;
+        if(kelvin>0)
+        {
+           far=1.8*(kelvin - 273.15) + 32;
+           celsius=kelvin -273.15;
+           printf("K: %.2f\nF: %f\nC: %.2f\n\n",kelvin, far, celsius);
+        }
+        else
+           printf("Not lower than absolute zero!\n\n");
+        celsius=temp;
+        if(celsius>-273)
+        {
+           kelvin=celsius+273.15;
+           far=1.8* celsius + 32;
+           printf("C: %.2f\nF: %.2f\nK: %.2f\n\n",celsius, far, kelvin);
+        }
+        else
+           printf("Not lower than absolute zero!\n\n");
+        far=temp;
+        if(far>-459)
+        {
+           celsius=5 * (far-32) / 9;
+           kelvin=(5 * (far-32) / 9) +273.15;
+           printf("F: %.2f\nK: %.2f\nC: %.2f",far, kelvin, celsius);
+        }
+        else
+           printf("Not lower than absolute zero!");
+
+    }
+    return 0;
+}
